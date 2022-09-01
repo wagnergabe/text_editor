1f0537393a58b5a45549defcbae7a7c0ef1dffc5
GSvenny <gabrielswagner7@gmail.com>
2022-09-01 16:12:17 -0500

    save install work/test

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -11,7 +11,14 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    
+    const Prompt_Event = window.deferredPrompt;
+    if(!Prompt_Event){
+        return;
+    }
+    Prompt_Event.prompt();
+    window.deferredPrompt = null;
+    butInstall.setAttribute("disabled", true);
+    butInstall.textContent = "Installed";
 });
 
 // TODO: Add an handler for the `appinstalled` event
